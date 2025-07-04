c954a1c0a181c18316798a85f45f7c7ff7e9b90a
johndocker22 <142202671+johndocker22@users.noreply.github.com>
2025-07-05 00:06:37 +0530

    putting failed case

--- maven-samples/single-module/src/test/java/com/example/TestGreeter.java
+++ maven-samples/single-module/src/test/java/com/example/TestGreeter.java
@@ -26,13 +26,18 @@ public class TestGreeter {
 
     assertThat(greeter.greet(someone), containsString(someone));
   }
+   @Test
+  public void intentionally_failed() {
+    String someone = "World";
+
+    assertThat(greeter.greet(someone), containsString(someone));
+  }
   
 
   @Test
   public void greetShouldIncludeGreetingPhrase() {
-    String someone = "World";
 
-    assertThat(greeter.greet(someone).length(), is(greaterThan(someone.length())));
+    assertThat(false);
   }
 
     @Test
