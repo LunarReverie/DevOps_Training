8834e59742e5a4502243a98459570c2897ee9e35
ibmbiosbit <ibmbiosbit@gmail.com>
2025-11-05 00:18:48 +0530

    Add intentionally failed test case

--- maven-samples/single-module/src/test/java/com/example/TestGreeter.java
+++ maven-samples/single-module/src/test/java/com/example/TestGreeter.java
@@ -27,7 +27,11 @@ public class TestGreeter {
     assertThat(greeter.greet(someone), containsString(someone));
   }
   
-
+@Test
+  public void intentionallyfailed() {
+    assertTrue(false);
+  }
+  
   @Test
   public void greetShouldIncludeGreetingPhrase() {
     String someone = "World";
